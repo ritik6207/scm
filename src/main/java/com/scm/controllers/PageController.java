09d9c040bd258254e2615acfab7c92fea9088d2e
Ritik Kumar <rk7382687@gmail.com>
2024-11-04 16:23:44 +0530

    setting up

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,20 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        System.out.println("Home page Handler");
+
+        // Sending data to view
+        model.addAttribute("name", "subString Technology");
+        model.addAttribute("youTube", "TechLearn");
+        model.addAttribute("gitHubRepo", "https://github.com/ritik6207");
+        return "Home";
+    }
+}
